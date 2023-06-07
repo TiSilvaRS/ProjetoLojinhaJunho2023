51f5afe4f56af5f4652b713844905e1851e9e1d8
Tiago Silva <tiagosilva@tiagosilva.com.br>
2023-06-07 15:56:22 -0300

    Lojinha07-06-2023 Mod6-Aula7

---
+++ src/main/java/br/com/lojinha/pojo/Produto.java
@@ -0,0 +1,54 @@
+package br.com.lojinha.pojo;
+import br.com.lojinha.enums.Tamanho;
+import java.util.List;
+
+public class Produto {
+        private String nome;
+        private String marca;
+        private double valor;
+        private Tamanho tamanho;
+        private List<String> itensInclusos;
+
+        public Produto(String marcaInicial, Tamanho tamanhoInicial) {
+            this.marca = marcaInicial;
+            this.tamanho = tamanhoInicial;
+        }
+
+        public double getValor() {
+                return valor;
+        }
+        public void setValor(double novoValor) {
+                if (novoValor > 0) {
+                        this.valor = novoValor;
+                } else {
+                        throw new IllegalArgumentException("O Valor não pode ser igual ou menor que 0");
+                }
+        }
+
+        public String getNome() {
+                return this.nome;
+        }
+        public void setNome(String novoNome) {
+                this.nome = novoNome;
+        }
+
+        public String getMarca() {
+                return this.marca;
+        }
+        public void setMarca(String novaMarca) { this.marca = novaMarca; }
+
+        public Tamanho getTamanho() {
+                return this.tamanho;
+        }
+        public void setTamanho(Tamanho novoTamanho) {
+        this.tamanho = novoTamanho;
+        }
+
+        public List<String> getItensInclusos() {
+                return itensInclusos;
+        }
+        public void setItensInclusos(List<String> novositensInclusos) {
+                this.itensInclusos = novositensInclusos;
+        }
+
+}
